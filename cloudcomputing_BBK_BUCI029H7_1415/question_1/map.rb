96f34ff6275f2fca0f321849b917b08b536671ff
dan-mi-sun <dan.mi.sun@gmail.com>
2015-01-07 13:30:44 +0000

    add algorithm overview pairs

---
+++ cloudcomputing_BBK_BUCI029H7_1415/question_1/map.rb
@@ -0,0 +1,15 @@
+# Ruby code for map.rb
+#
+# w denotes the target word
+# w` denotes the neighbour of w
+
+#Read in each line of text.
+ARGF.each do |word|
+
+  #0. Start at the first word.
+  #1. Check is the word is 'w'
+  #2a. If yes emit (pair(w,w`), count 1)
+  #2aa. => then move to the next word & repeat step 1
+  #2b. If not then move to the next word & repeat step 1
+
+end

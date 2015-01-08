d80b370f2c219dfd2309a9989cd3ebd109fc353b
dan-mi-sun <dan.mi.sun@gmail.com>
2015-01-08 11:28:47 +0000

    start of stripes reduce script

---
+++ cloudcomputing_BBK_BUCI029H7_1415/question_1/stripes/reduce.rb
@@ -0,0 +1,9 @@
+require 'pry-byebug'
+# Ruby code for Stripes method map.rb
+#
+
+#Read in each line of text.
+# ARGF.each do |line|
+#
+#Test code locally
+f = File.open("/Users/dan_mi_sun/projects/bbk_actmasters_14-15/cloudcomputing_BBK_BUCI029H7_1415/question_1/simple-words-test.txt", "r")

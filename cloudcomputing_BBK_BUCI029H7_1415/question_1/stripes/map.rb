23b75ebded4bc180255e565378c57ef966c93639
dan-mi-sun <dan.mi.sun@gmail.com>
2015-01-08 11:16:47 +0000

    implement ending play to iterator for word pairs

--- cloudcomputing_BBK_BUCI029H7_1415/question_1/stripes/map.rb
+++ cloudcomputing_BBK_BUCI029H7_1415/question_1/stripes/map.rb
@@ -12,34 +12,36 @@ f = File.open("/Users/dan_mi_sun/projects/bbk_actmasters_14-15/cloudcomputing_BB
 f.each_line do |line|
   #Create Associative Array
   h = {}
+  #remove new line character
   line = line.chomp
   #split line into array of words
   word_array = line.split(' ')
   #iterate over array of words
   word_array.each_with_index {|word, i|
-    #change word to lower case
-    word = word.downcase
-    #assign neighbour
-    neighbour = word_array[i + 1] unless i = (word_array.length) - 2
-    #catch error if neighbour is nil
-    # next if neighbour.nil?
-
-    #if hash has word key
-    if h.has_key?(word) 
-      #if word key has neighbour
-      if h[word].has_key?(neighbour)
-        #find out current value of neighbour
-        v = h[word][neighbour] 
-        #add 1 to neighbour count
-        h[word] = {neighbour => (v + 1) } 
-        #else add neighbour key & set count at 1
+    #check to see if we should duck out the interation and return the associative hash
+    if i == (word_array.size) -1 
+      print h
+    else
+      #change word to lower case
+      word = word.downcase
+      #assign neighbour
+      neighbour = word_array[i + 1] 
+      #if hash has word key
+      if h.has_key?(word) 
+        #and if word key has neighbour
+        if h[word].has_key?(neighbour)
+          #find out current value of neighbour
+          v = h[word][neighbour] 
+          #add 1 to neighbour count
+          h[word] = {neighbour => (v + 1) } 
+          #else add neighbour key & set count at 1
+        else 
+          h[word] = {neighbour => 1}
+        end
+        #else add word with {} && add neighbour key && set count at 1
       else 
-        h[word] = {neighbour => 1}
+        h[word] = {} && h[word] = {neighbour => 1}
       end
-      #else add word with {} && add neighbour key && set count at 1
-    else 
-      h[word] = {} && h[word] = {neighbour => 1}
     end
   }
-print h
 end

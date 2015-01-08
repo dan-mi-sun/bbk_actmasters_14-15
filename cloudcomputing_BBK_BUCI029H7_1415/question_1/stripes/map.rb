01960d4f607434f6e8804c3e4cf49cbfdd7c65d5
dan-mi-sun <dan.mi.sun@gmail.com>
2015-01-08 23:23:00 +0000

    add comment spacing

--- cloudcomputing_BBK_BUCI029H7_1415/question_1/stripes/map.rb
+++ cloudcomputing_BBK_BUCI029H7_1415/question_1/stripes/map.rb
@@ -11,36 +11,48 @@ ARGF.each do |text|
   #0.Split text into lines 
   # f.each_line do |text|
   #Create Associative Array
+  #
   h = {}
-  #split text into paragraphs
+  #split text into paragraphs and convert all words to lowercase
+  #
   paragraph = text.split("\n").map!{ |w| w.downcase}
   #remove non-alphanumeric characters and create a word array
+  #
   if paragraph.size == 0 
     word_array = ['']
   else
     word_array = paragraph[0].gsub!(/[^0-9a-z ]/i, '').split(' ')
   end
   #iterate over array of words
+  #
   word_array.each_with_index {|word, i|
     #check to see if we should duck out the interation and return the associative hash
+    #
     if i == (word_array.size) -1 
       break
     else
       #assign neighbour
+      #
       neighbour = word_array[i + 1] 
       #if hash has word key
+      #
       if h.has_key?(word) 
         #and if word key has neighbour
+        #
         if h[word].has_key?(neighbour)
           #find out current value of neighbour
+          #
           v = h[word][neighbour] 
           #add 1 to neighbour count
+          #
           h[word] = {neighbour => (v + 1) } 
           #else append neighbour key & set count at 1
+          #
         else 
           h[word].merge!(neighbour => 1)
         end
         #else add word with {} && add neighbour key && set count at 1
+        #
       else 
         h[word] = {} && h[word] = {neighbour => 1}
       end
@@ -48,6 +60,7 @@ ARGF.each do |text|
   }
   #output to STDOUT
   #<key><tab><value><newline>
+  #
   h_array = h.to_a
   h_array.each { |pairs|
     key = pairs[0]

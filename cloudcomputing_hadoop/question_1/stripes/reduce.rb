41609461087039f14ce6a572ab5538593d40720c
dan-mi-sun <dan.mi.sun@gmail.com>
2015-01-12 08:01:21 +0000

    tidy up comments on reducer

--- cloudcomputing_hadoop/question_1/stripes/reduce.rb
+++ cloudcomputing_hadoop/question_1/stripes/reduce.rb
@@ -1,13 +1,8 @@
-require 'pry-byebug'
-
 # Ruby code for Stripes method reduce.rb
-#
 
 #create data structure within which to do internal corresponding value addition
-#
 merging_container = {}
 
-#Read in each line of text.
 ARGF.each do |line|
 
   # remove any newline
@@ -15,7 +10,6 @@ ARGF.each do |line|
 
   # split key and value on tab character
   (key, value) = line.split(/\t/)
-
 
   #remove non-alphanumeric chars from value string and turn into array
   a = value.gsub!(/[^0-9a-z ]/i, ' ').split(' ')
@@ -25,18 +19,14 @@ ARGF.each do |line|
 
   #set top level key for use within inner hash loop
   _key = key
-  #if the key already exists then append
-  #
-  if merging_container.has_key?(key)
 
-    #look to see if the h contains an existing inner key with an additional value 
-    #
+  if merging_container.has_key?(key)
+    #look to see if the hash_map contains an existing inner key with an additional value 
     hash_map.each do |key, value|
 
       if merging_container[_key].has_key?(key)
         #if key exists in inner hash then add the value to the existing entry within the merging container
         merging_container[_key][key] =  merging_container[_key][key].to_i + value.to_i
-
       else
         merging_container[_key].merge!(key => value)
       end
@@ -44,19 +34,12 @@ ARGF.each do |line|
 
   else
     #if the key does not already exist then make new entry
-    #add key and values to merging container
-
-    #add key value pairs to data structure
     hash_map.each do |key, value|
       #add top level key to merging container
-      #
       merging_container[_key] ||= {}
 
       #add inner key and value to top level key
-      #
       merging_container[_key].merge!(key => value)
-      # merging_container[_key][key] = value
-
     end
   end
 end

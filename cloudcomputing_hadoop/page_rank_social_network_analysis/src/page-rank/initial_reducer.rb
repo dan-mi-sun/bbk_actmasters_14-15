bedd1cde82b05148d13660f165cbef8e4fc24ab6
dan-mi-sun <dan.mi.sun@gmail.com>
2015-01-22 07:42:43 +0000

    reset counter for new people and rename to network map

--- cloudcomputing_hadoop/page_rank_social_network_analysis/src/page-rank/initial_reducer.rb
+++ cloudcomputing_hadoop/page_rank_social_network_analysis/src/page-rank/initial_reducer.rb
@@ -13,8 +13,8 @@ class Network
     "/Users/dan_mi_sun/projects/msc_advanced_computing_technologies/cloudcomputing_hadoop/page_rank_social_network_analysis/src/page-rank/output.txt"
   end
 
-  def output_recommendations
-    adjacency_list = {}
+  def create_map
+    network_map = {}
     pagerank = 1
     outlink = 1
 
@@ -23,26 +23,27 @@ class Network
       trusts = r.chomp!
       (key, value) =  trusts.split(/\t/)
 
-      if adjacency_list.has_key?(key)
+      if network_map.has_key?(key)
         #{person-key,{PR,{#out-links,[adjacency-list]}}
         #if it has the key then add it on to the end 
         #increase outlink counter by one
         outlink = outlink += 1
-        temp = adjacency_list[key][pagerank]
+        temp = network_map[key][pagerank]
         #change outlink counter key
-        adjacency_list[key][pagerank] = temp.inject({ }){ |x, (k,v)| x[k = outlink] = v; x }
+        network_map[key][pagerank] = temp.inject({ }){ |x, (k,v)| x[k = outlink] = v; x }
         #append the destination (value) to the adjacencylist array
-        adjacency_list[key][pagerank][outlink].push(value)
+        network_map[key][pagerank][outlink].push(value)
         # puts "this is the if with key #{key}"
       else
         #{person-key,{PR,{#out-links,[adjacency-list]}}
-        adjacency_list[key] ||= {}
-        adjacency_list[key].merge!(pagerank => {})
-        adjacency_list[key][pagerank].merge!(outlink => [value])
+        outlink = 1
+        network_map[key] ||= {}
+        network_map[key].merge!(pagerank => {})
+        network_map[key][pagerank].merge!(outlink => [value])
         # puts "this is the else"
       end
     end
-    return adjacency_list
+    return network_map
   end
 end
 
@@ -65,6 +66,7 @@ end
 
 #KEY:  gathers person keys
 #
+#{person-key,{PR,{#out-links,[adjacency-list]}}
 #VALUE: creates adjacency list for trusts (links-out)
 # => calculates total number of trusts for person
 # => logs pagerank for person
@@ -73,7 +75,3 @@ end
 # => logs total_nodes (N) - #_of_danglers
 
 
-#{person-key,{PR,{#out-links,[adjacency-list]}}
-
-p = Person.new
-p.create_adjacency_list
